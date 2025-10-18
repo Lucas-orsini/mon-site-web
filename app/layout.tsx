79bc171fe050adaedb00f631f2c4cfb517b3c01d
Lucas Orsini <lucas.orsini@outlook.fr>
2025-10-18 17:01:46 +0200

    modif favicon black

--- app/layout.tsx
+++ app/layout.tsx
@@ -69,13 +69,14 @@ export const metadata: Metadata = {
     images: [`${siteUrl}/logo-lovisuals.png`],
   },
   icons: {
-    icon: [
-      { url: "/favicon.ico", sizes: "any" },
-      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
-      { url: "/favicon-48x48.png", sizes: "48x48", type: "image/png" },
+     icon: [
+      { url: "/favicon-black.ico", sizes: "any" },
+      { url: "/favicon-32x32-black.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon-48x48-black.png", sizes: "48x48", type: "image/png" },
     ],
-    apple: "/apple-touch-icon.png",
+    apple: "/apple-touch-icon-black.png",
   },
+  
 }
 
 export default function RootLayout({
